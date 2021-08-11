c884d082a45ec1e03fac12a3e3806707dd9d53ec
Marcin Rataj <lidel@lidel.org>
2021-08-11 18:09:09 +0200

    fix: no raw leaves
    We want to reuse pinning provided by Fleek, but atm it uses CIDv0,
    which does not support raw leaves.
    
    By disabling raw leaves in CIDv1 add, we get the same underlying dag-pb,
    and the same multihash inside of both CIDv1 used in tests and CIDv0
    produced by Fleek.

--- constants.js
+++ constants.js
@@ -1,8 +1,7 @@
 // Constants for test files, generated with publish-files.js
 
 // Raw hashes
-<<<<<<< HEAD
-export const URL_IPFS_MEDIA = 'ipfs://bafybeievhgvy6xq64x32fufpkwkhly32au7pkdmczdgyypg435w6ftmet4/'
+export const URL_IPFS_MEDIA = 'ipfs://bafybeic7n7s52drjp67yjklbiyxyzh3ddw35qr4wcuxkra6qmu37czxrmi/'
 export const URL_IPFS_TEXT_FILE_RAW = 'ipfs://bafybeiduiecxoeiqs3gyc6r7v3lymmhserldnpw62qjnhmqsulqjxjmtzi/?filename=files/example.txt'
 export const URL_IPFS_IMAGE_FILE_RAW = 'ipfs://bafybeihc4hti5ix4ds2tefhy35qd4c7n5as5cazdmksrxj7ipvcxm64h54/?filename=files/ipfs-logo.svg'
 export const URL_IPFS_VIDEO_FILE_RAW = 'ipfs://bafybeifmi5ykqzw4xnyarcx4wltdqid3ivacz3dr5qobskrajprs53uvj4/?filename=files/IPFS.mp4'
@@ -11,17 +10,6 @@ export const URL_IPFS_JS_FILE_RAW = 'ipfs://bafybeie6onxucug6vpresqyspluul3dp5ln
 export const URL_IPFS_CSS_FILE_RAW = 'ipfs://bafybeib7jtnjgsqnws3yt2wyw6up54tl2y4h4gwergruhdl4l4ehd57y4y/?filename=files/example.css'
 export const URL_IPFS_HTML_FILE_RAW = 'ipfs://bafybeibsjyvcryciw666lqypkbgr4q5bvnmiipdsbw53hratenbvyoeevm/?filename=files/example.html'
 export const URL_IPFS_REDIRECT_FILE_RAW = 'ipfs://bafybeigoxuborlvdwjel4xw4w5bxl4ucse5uac3jnhdm34zrhe3ynmev6e/?filename=files/redirect.html'
-=======
-export const URL_IPFS_MEDIA = 'ipfs://bafybeidojbnpcrx3hrensu75uarng2jsp4xnen56qrwtl7qeij4456d2au/'
-export const URL_IPFS_TEXT_FILE_RAW = 'ipfs://bafkreigsvbhuxc3fbe36zd3tzwf6fr2k3vnjcg5gjxzhiwhnqiu5vackey/?filename=files/example.txt'
-export const URL_IPFS_IMAGE_FILE_RAW = 'ipfs://bafkreicncneocapbypwwe3gl47bzvr3pkpxmmobzn7zr2iaz67df4kjeiq/?filename=files/ipfs-logo.svg'
-export const URL_IPFS_VIDEO_FILE_RAW = 'ipfs://bafybeibuad3l4p6g7n3mqywkfshxzorlpkdfzcuaczzruqvudo5eqgarhi/?filename=files/IPFS.mp4'
-export const URL_IPFS_AUDIO_FILE_RAW = 'ipfs://bafkreicjcu4fn5n57psz7iefp63jzdtcogd65iwtq33da6mtyvgxfhg7pa/?filename=files/ipfs.mp3'
-export const URL_IPFS_JS_FILE_RAW = 'ipfs://bafkreiff66rjfncaw7eynzlpw2cxxcykierusq4v5cs6aurb7o6yc3a2fu/?filename=files/example.js'
-export const URL_IPFS_CSS_FILE_RAW = 'ipfs://bafkreifqlxljadxls4doufn5dzdp6zivzowmoqhhvcuf7ldjyprd3v25lm/?filename=files/example.css'
-export const URL_IPFS_HTML_FILE_RAW = 'ipfs://bafkreiai7j3f4nibam6n5stzh2ew3s4w7pzjz4npgzvlzfs6m2phgufvhi/?filename=files/example.html'
-export const URL_IPFS_REDIRECT_FILE_RAW = 'ipfs://bafkreiehlqlhwhcf2vfert526hjwoizgh3ekdj5hhgn6twt3pyeu6xtoeq/?filename=files/redirect.html'
->>>>>>> a33804c (wip: prep for fleek publishing)
 
 // Relative paths
 export const URL_IPFS_TEXT_FILE = `${URL_IPFS_MEDIA}example.txt`
@@ -37,11 +25,7 @@ export const URL_IPFS_DIRECTORY_NO_INDEX = `${URL_IPFS_MEDIA}`
 export const URL_IPFS_DIRECTORY_WITH_INDEX = `${URL_IPFS_MEDIA}with-index/`
 export const URL_IPFS_DIRECTORY_EMPTY = `${URL_IPFS_MEDIA}empty`
 export const URL_IPFS_REDIRECT_FILE = `${URL_IPFS_MEDIA}redirect.html`
-<<<<<<< HEAD
 export const URL_IPNS_MEDIA = `ipns://ipfs-protocol-compliance-suite.on.fleek.co/files/`
-=======
-export const URL_IPNS_MEDIA = `ipns://ipfs-protocol-compliance-suite.on.fleek.co/`
->>>>>>> a33804c (wip: prep for fleek publishing)
 export const URL_IPNS_TEXT_FILE = `${URL_IPNS_MEDIA}example.txt`
 export const URL_IPNS_IMAGE_FILE = `${URL_IPNS_MEDIA}ipfs-logo.svg`
 export const URL_IPNS_VIDEO_FILE = `${URL_IPNS_MEDIA}IPFS.mp4`
